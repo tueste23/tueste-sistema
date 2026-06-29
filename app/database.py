5068f7bb7dabe095ef526ff1ad28dc3694b35607
Vanesa Defey Sosa <vanu@MacBook-Pro-de-Vanesa.local>
2026-06-29 16:57:43 -0300

    fix db path

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tueste.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/tueste.db")
 
 # Railway usa PostgreSQL, localmente usamos SQLite
 if DATABASE_URL.startswith("postgres://"):
